8d486a272c0f3ba0748f8947b61937ef1dc8eec3
Ivan <zrosji@gmail.com>
2024-03-13 23:24:53 +0100

    auth header links

--- src/app/feature/auth/components/auth-header/auth-header.component.ts
+++ src/app/feature/auth/components/auth-header/auth-header.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LogoComponent } from '../../../../shared/layout/header/components/logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-header',
   standalone: true,
   template: `
     <header>
-      <app-logo></app-logo>
+      <div class="container">
+        <app-logo></app-logo>
+        <div class="btns" style="gap: 20px; display: flex;">
+          <a class="switch-auth-method" routerLink="login">Login</a>
+          <a class="switch-auth-method" routerLink="register">Register</a>
+        </div>
+      </div>
     </header>
   `,
   styleUrl: './auth-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, LogoComponent],
+  imports: [CommonModule, LogoComponent, RouterLink],
 })
 export class AuthHeaderComponent {}
