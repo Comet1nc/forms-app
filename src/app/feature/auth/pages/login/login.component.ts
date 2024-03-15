a7910c0a64c9c5c6990a37a3619b6151948300e8
Ivan <zrosji@gmail.com>
2024-03-15 18:42:45 +0100

    login form

--- src/app/feature/auth/pages/login/login.component.ts
+++ src/app/feature/auth/pages/login/login.component.ts
@@ -4,7 +4,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  template: `login`,
+  template: ` <div class="form-container">
+    <input placeholder="E-mail:" type="text" />
+    <input placeholder="Password: " type="text" />
+    <div class="remember-me">
+      <input class="checkbox" type="checkbox" name="remember-me" />
+      <span>Remember me</span>
+    </div>
+    <a class="submit" href="#">Login</a>
+  </div>`,
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
--- src/app/feature/auth/pages/register/register.component.ts
+++ src/app/feature/auth/pages/register/register.component.ts
@@ -5,7 +5,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>register works!</p>`,
+  template: `
+    <div class="form-container">
+      <input type="text" />
+      <input type="text" />
+      <div class="remember-me">
+        <input type="checkbox" name="remember-me" />
+        <span>Remember Me</span>
+      </div>
+      <a href="#">Register</a>
+    </div>
+  `,
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
